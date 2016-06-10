d90da7a3d2f0fa121653cf4158f0f882ab22ed83
Jonathan Dexter <jdexter@nerdery.com>
2016-06-10 17:09:34 -0500

    Prepare for release 0.0.5-beta.

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 0.0.5-beta - June 10th, 2016
+* Allow for non-Guid keyed IReliableDictionaries to be utilized.
+
 #### 0.0.4-beta - June 10th, 2016
 * Initial Release
--- src/FSFabric/AssemblyInfo.fs
+++ src/FSFabric/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSFabric")>]
 [<assembly: AssemblyProductAttribute("FSFabric")>]
 [<assembly: AssemblyDescriptionAttribute("A set of modules and functions for working with Service Fabric in F#.")>]
-[<assembly: AssemblyVersionAttribute("0.0.4")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
+[<assembly: AssemblyVersionAttribute("0.0.5")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.4"
-    let [<Literal>] InformationalVersion = "0.0.4"
+    let [<Literal>] Version = "0.0.5"
+    let [<Literal>] InformationalVersion = "0.0.5"
