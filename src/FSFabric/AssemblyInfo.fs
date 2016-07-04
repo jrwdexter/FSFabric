3fbec776ab26d1d1f10fbad4b3115706e4401ee8
mandest <mandest@gmail.com>
2016-07-03 20:12:24 -0500

    Updated project file.

--- src/FSFabric/AssemblyInfo.fs
+++ src/FSFabric/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSFabric")>]
 [<assembly: AssemblyProductAttribute("FSFabric")>]
 [<assembly: AssemblyDescriptionAttribute("A set of modules and functions for working with Service Fabric in F#.")>]
-[<assembly: AssemblyVersionAttribute("0.0.7")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.7")>]
+[<assembly: AssemblyVersionAttribute("0.0.8")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.8")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.7"
-    let [<Literal>] InformationalVersion = "0.0.7"
+    let [<Literal>] Version = "0.0.8"
+    let [<Literal>] InformationalVersion = "0.0.8"
